4be087fcd21ebb532903cb172e7edbc4b4c36081
Jorge <jorge.bravo@live.u-tad.com>
2022-02-22 20:50:24 +0100

    ssssssss

--- app/src/main/java/com/api/Peliculas/datos/ProductObjectItem.kt
+++ app/src/main/java/com/api/Peliculas/datos/ProductObjectItem.kt
@@ -4,7 +4,8 @@ package com.api.Peliculas.datos
 import com.google.gson.annotations.SerializedName
 
 data class ProductObjectItem(
-
+    @SerializedName("pack")
+    val pack: String,
     @SerializedName("description")
     val description: String,
     @SerializedName("_id")
--- app/src/main/java/com/api/Peliculas/datos/ProductObjectRequest.kt
+++ app/src/main/java/com/api/Peliculas/datos/ProductObjectRequest.kt
@@ -3,7 +3,8 @@ package com.api.Peliculas.datos
 import com.google.gson.annotations.SerializedName
 
 data class ProductObjectRequest(
-
+    @SerializedName("pack")
+    val pack: String,
     @SerializedName("description")
     val description: String,
     @SerializedName("name")
