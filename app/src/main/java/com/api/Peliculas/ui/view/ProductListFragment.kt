5d2b16bb65006b311c84eb16770daa02a80643b0
Jorge <jorge.bravo@live.u-tad.com>
2022-03-11 14:34:25 +0100

    Cambios para recu

--- app/src/main/java/com/api/Peliculas/ui/view/ProductListFragment.kt
+++ app/src/main/java/com/api/Peliculas/ui/view/ProductListFragment.kt
@@ -1,5 +1,6 @@
 package com.api.Peliculas.ui.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -63,6 +64,7 @@ class ProductListFragment : Fragment() {
     }
 
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
